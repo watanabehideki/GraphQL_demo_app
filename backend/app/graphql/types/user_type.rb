0f62f4c371ce01f0d8178db0af56b1ab0f318b8a
watanabehideki <hideki.art.2280@gmail.com>
2021-10-17 14:13:42 +0900

    first commit

---
+++ backend/app/graphql/types/user_type.rb
@@ -0,0 +1,15 @@
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :email, String, null: true
+    field :name, String, null: true
+    field :posts, [Types::PostType], null: true
+    field :posts_count, Integer, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def posts_count
+      object.posts.size
+    end
+  end
+end
